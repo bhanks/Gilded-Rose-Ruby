ab3ca5b6ed4e3d8f01247f6b0f3fdbaf582d0708
Brendan Hanks <bhanks@brendans-mbp.lan>
2022-03-31 17:27:53 -0500

    Code interview with Brendan Hanks

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -1,55 +1,94 @@
 class GildedRose
-  attr_reader :name, :days_remaining, :quality
+  attr_reader :name, :days_remaining, :quality, :type
 
   def initialize(name:, days_remaining:, quality:)
     @name = name
     @days_remaining = days_remaining
     @quality = quality
+    case @name
+    when "Aged Brie"
+      @type = :aged
+    when "Backstage passes to a TAFKAL80ETC concert"
+      @type = :backstage
+    when "Legendary Sulfuras, Hand of Ragnaros"
+      @type = :legendary
+    end
   end
 
   def tick
-    if @name != "Aged Brie" and @name != "Backstage passes to a TAFKAL80ETC concert"
-      if @quality > 0
-        if @name != "Legendary Sulfuras, Hand of Ragnaros"
-          @quality = @quality - 1
-        end
-      end
+    case @type
+    when :aged
+      tick_aged
+    when :legendary
+      tick_legendary
+    when :backstage
+      tick_backstage
     else
-      if @quality < 50
-        @quality = @quality + 1
-        if @name == "Backstage passes to a TAFKAL80ETC concert"
-          if @days_remaining < 11
-            if @quality < 50
-              @quality = @quality + 1
-            end
-          end
-          if @days_remaining < 6
-            if @quality < 50
-              @quality = @quality + 1
-            end
-          end
-        end
-      end
+      tick_normal
     end
-    if @name != "Legendary Sulfuras, Hand of Ragnaros"
-      @days_remaining = @days_remaining - 1
-    end
-    if @days_remaining < 0
-      if @name != "Aged Brie"
-        if @name != "Backstage passes to a TAFKAL80ETC concert"
-          if @quality > 0
-            if @name != "Legendary Sulfuras, Hand of Ragnaros"
-              @quality = @quality - 1
-            end
-          end
-        else
-          @quality = @quality - @quality
-        end
-      else
-        if @quality < 50
-          @quality = @quality + 1
-        end
-      end
-    end
+
   end
+
+  def tick_aged
+    if @quality < 50
+      @quality += 1
+    end
+
+    @days_remaining -= 1
+
+    if @days_remaining < 0 && @quality < 50
+      @quality += 1
+    end
+    
+  end
+
+  def tick_legendary
+    # Future Legendary tick
+  end
+
+  def tick_backstage
+
+    if @quality < 50
+      @quality += 1 
+    end
+
+
+    if @days_remaining < 11
+      if @quality < 50
+        @quality += 1 
+      end
+    end
+
+    if @days_remaining < 6
+      if @quality < 50
+        @quality += 1 
+      end
+    end
+
+    @days_remaining -= 1
+
+    if @days_remaining < 0
+      @quality = 0
+    end
+
+
+  end
+
+  def tick_normal
+    if @quality > 0
+      @quality -= 1 
+    end
+
+    @days_remaining -= 1 
+
+    if @days_remaining < 0
+      if @quality > 0
+        @quality -= 1 
+      end
+    end
+
+
+  end
+
 end
+
